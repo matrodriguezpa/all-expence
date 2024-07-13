dd6307146257d1321fec8ed640bf43ed5b0cbd5e
Teo <144364977+MatRodriguezPa@users.noreply.github.com>
2024-07-12 23:02:01 -0500

    refactoring

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+
+import View.*;
+import Controller.*;
+import Model.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Screen myScreen = new Screen();
+        myScreen.setVisible(true);
+    }
+
+}
